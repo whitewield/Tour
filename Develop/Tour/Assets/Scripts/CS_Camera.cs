fbcd09acc3714eb6ce30212d04b08d3a93ee786c
HANG-PC <whitewield@gmail.com>
2018-11-04 11:10:36 -0500

    Camera zoom out to see the whole scene

--- Develop/Tour/Assets/Scripts/CS_Camera.cs
+++ Develop/Tour/Assets/Scripts/CS_Camera.cs
@@ -11,8 +11,17 @@ public class CS_Camera : MonoBehaviour {
 	private Camera myCamera;
 
 	private float myHeight = 10f;
-	[SerializeField] float myPlayerForwardRatio;
-	[SerializeField] float myLerpSpeed = 1;
+
+	[SerializeField] float myPlayerForwardRatio = 1.26f;
+	[SerializeField] Vector3 myTargetPosition;
+	[SerializeField] float myMoveLerpSpeed = 1.8f;
+
+	[SerializeField] bool doFollowPlayer = true;
+	[SerializeField] float myTargetSize = 5;
+	private float myFullSize;
+	[SerializeField] float mySizeLerpSpeed = 1;
+	[SerializeField] float mySizeFullRatio = 1.2f;
+
 
 	private void Awake () {
 		if (instance != null && instance != this) {
@@ -23,20 +32,63 @@ public class CS_Camera : MonoBehaviour {
 		}
 
 		myCamera = this.GetComponent<Camera> ();
-	}
 
-	// Use this for initialization
-	void Start () {
-		myPlayer = CS_PlayerControl.Instance;
+		// get the default height of camera
+		myHeight = this.transform.position.y;
 	}
 	
-	// Update is called once per frame
+	void Start () {
+		// find the player instance
+		myPlayer = CS_PlayerControl.Instance;
+
+		// calculate the full size according to map
+		myFullSize = CalculateFullSize ();
+	}
+
+	// calculate the full size according to map
+	private float CalculateFullSize () {
+		// get back ground local scale
+		Vector3 t_mapScale = CS_Map.Instance.BackGroundTransform.localScale;
+		// calculate the map world scale to screen scale
+		t_mapScale.y *= 0.5f;
+
+		// map aspect
+		float t_mapAspect = t_mapScale.x / t_mapScale.y;
+
+		// if the camera aspect is larger than map aspect
+		if (myCamera.aspect > t_mapAspect) {
+			return t_mapScale.y * 0.5f * mySizeFullRatio;
+		}
+
+		// if the camera aspect is smaller than maps aspect
+		float t_targetHeight = 1 / myCamera.aspect * t_mapScale.x;
+		return t_targetHeight * 0.5f * mySizeFullRatio;
+	}
+	
 	void Update () {
-		Vector3 t_targetPosition =
+		if (doFollowPlayer) {
+			Update_FollowPlayer ();
+		} else {
+			Update_FullView ();
+		}
+
+		// update the position
+		this.transform.position = Vector3.Lerp (this.transform.position, myTargetPosition, Time.deltaTime * myMoveLerpSpeed);
+		// update the size
+		myCamera.orthographicSize = Mathf.Lerp (myCamera.orthographicSize, myTargetSize, Time.deltaTime * mySizeLerpSpeed);
+	}
+
+	private void Update_FullView () {
+		myTargetPosition = new Vector3 (0, myHeight, -myHeight * 1.73f);
+
+		myTargetSize = CalculateFullSize ();
+	}
+
+	private void Update_FollowPlayer () {
+		// calculate target position according to player's move direction
+		myTargetPosition =
 			myPlayer.transform.position +
 					myPlayer.MyDirection * myPlayerForwardRatio * myCamera.orthographicSize +
 					new Vector3 (0, myHeight, -myHeight * 1.73f);
-
-		this.transform.position = Vector3.Lerp (this.transform.position, t_targetPosition, Time.deltaTime * myLerpSpeed);
 	}
 }
---
+++ Develop/Tour/Assets/Scripts/CS_Map.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Map : MonoBehaviour {
+
+	private static CS_Map instance = null;
+	public static CS_Map Instance { get { return instance; } }
+
+	[SerializeField] Transform myBackGroundTransform;
+	public Transform BackGroundTransform { get { return myBackGroundTransform; } }
+
+	private void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
